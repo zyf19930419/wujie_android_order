4ac2fa4ed509ded11d785d73e84712fd4c25e49e
Quyl <aliang_0223@163.com>
2018-07-03 11:18:50 +0800

    添加首页底部按钮，以及消息页面，消息列表

---
+++ app/src/main/java/com/wujiemall/order/adapter/MsgAdapter.java
@@ -0,0 +1,56 @@
+package com.wujiemall.order.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wujiemall.order.R;
+
+import java.util.List;
+
+/**
+ * 创建者：Qyl
+ * 创建时间：2018/7/3 0003 11:02
+ * 功能描述：首页消息页面列表适配器
+ * 联系方式：无
+ */
+public class MsgAdapter extends RecyclerView.Adapter {
+    private Context context;
+    private List list;
+
+    public MsgAdapter(Context context, List list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        MyViewHolder holder = new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_msg_lv_item, parent, false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView times, msgDetails;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            times = itemView.findViewById(R.id.msg_item_time);
+            msgDetails = itemView.findViewById(R.id.msg_item_details);
+        }
+    }
+}
--- app/src/main/java/com/wujiemall/order/ui/MainActivity.java
+++ app/src/main/java/com/wujiemall/order/ui/MainActivity.java
@@ -2,11 +2,15 @@ package com.wujiemall.order.ui;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -14,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wujiemall.order.R;
+import com.wujiemall.order.adapter.MsgAdapter;
 import com.wujiemall.order.adapter.MyPagerAdapter;
 import com.wujiemall.order.base.BaseActivity;
 import com.wujiemall.order.fragment.MainRowNumber;
@@ -32,9 +37,41 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private ViewPager rowNumberVp;
     private ArrayList<String> strings;
     private ArrayList<Fragment> fragments;
+    /**
+     * 首页列表适配器
+     */
     private MyPagerAdapter adapter;
     private RelativeLayout titleBavk;
+    /**
+     * 返回按钮
+     */
     private ImageView imagerBack;
+    /**
+     * 首页填充布局
+     */
+    private FrameLayout frglayout;
+    /**
+     * 排号布局
+     */
+    private View rowNumber;
+    /**
+     * 被添加的消息列表页面
+     */
+    private View atyMsg;
+    /**
+     * 排号按钮
+     */
+    private TextView numberButton;
+    /**
+     * 堂点按钮
+     */
+    private TextView parishButton;
+    /**
+     * 消息列表
+     */
+    private TextView msgButton;
+    private ArrayList<TextView> listViews;
+    private RecyclerView reList;
 
     @Override
     public int getLayoutId() {
@@ -45,10 +82,28 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void initView() {
 
         ToastUitl.show("" + isNetConnect(), Toast.LENGTH_SHORT);
+
+        frglayout = findViewById(R.id.activity_frg);
+        numberButton = findViewById(R.id.number_button);
+        parishButton = findViewById(R.id.parish_button);
+        msgButton = findViewById(R.id.msg_button);
+        listViews = new ArrayList<>();
+        listViews.add(numberButton);
+        listViews.add(parishButton);
+        listViews.add(msgButton);
+        //被添加的消息列表页面
+        atyMsg = View.inflate(this, R.layout.activity_msg, null);
+        reList = atyMsg.findViewById(R.id.acrivity_msg_relist);
+        reList.setLayoutManager(new LinearLayoutManager(this));
+        MsgAdapter msgAdapter = new MsgAdapter(this, listViews);
+        reList.setAdapter(msgAdapter);
+        //被添加布局排号
+        rowNumber = View.inflate(this, R.layout.activity_rownumber, null);
+        tabLayout = rowNumber.findViewById(R.id.aty_tablayout);
+        rowNumberVp = rowNumber.findViewById(R.id.aty_row_number_vp);
+        //页面title
         titleName = findViewById(R.id.aty_title_name);
         titleRigth = findViewById(R.id.aty_title_rigth);
-        tabLayout = findViewById(R.id.aty_tablayout);
-        rowNumberVp = findViewById(R.id.aty_row_number_vp);
         titleBavk = findViewById(R.id.title_re_layout);
         imagerBack = findViewById(R.id.aty_title_back);
 
@@ -57,7 +112,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         tabLayout.setupWithViewPager(rowNumberVp);
         titleName.setText("RED COCK(花苑店)");
         titleName.setTextColor(Color.WHITE);
-
+        frglayout.addView(rowNumber);
         titleRigth.setVisibility(View.VISIBLE);
         titleRigth.setText("打号");
         strings = new ArrayList<>();
@@ -71,7 +126,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         adapter = new MyPagerAdapter(getSupportFragmentManager(), fragments, strings);
 
         rowNumberVp.setAdapter(adapter);
+        //注册点击事件
         titleRigth.setOnClickListener(this);
+        numberButton.setOnClickListener(this);
+        parishButton.setOnClickListener(this);
+        msgButton.setOnClickListener(this);
     }
 
     @Override
@@ -91,6 +150,37 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             case R.id.aty_title_rigth:
                 startActivity(AtyNumbering.class);
                 break;
+            case R.id.number_button:
+                frglayout.removeAllViews();
+                frglayout.addView(rowNumber);
+                setButtons(numberButton);
+                break;
+            case R.id.parish_button:
+                setButtons(parishButton);
+                break;
+            case R.id.msg_button:
+                frglayout.removeAllViews();
+                frglayout.addView(atyMsg);
+                setButtons(msgButton);
+                break;
+        }
+    }
+
+    private void setButtons(TextView view) {
+        if (listViews != null) {
+            for (int i = 0; i < listViews.size(); i++) {
+                if (listViews.get(i) == view) {
+                    listViews.get(i).setTextColor(getResources().getColor(R.color.title_redF23030));
+                    Drawable drawable = getResources().getDrawable(R.drawable.ic_success);
+                    drawable.setBounds(drawable.getMinimumWidth(), 0, 0, drawable.getMinimumHeight());
+                    listViews.get(i).setCompoundDrawables(null, drawable, null, null);
+                } else {
+                    listViews.get(i).setTextColor(getResources().getColor(R.color.f999999));
+                    Drawable drawable = getResources().getDrawable(R.drawable.icon_be_back);
+                    drawable.setBounds(drawable.getMinimumWidth(), 0, 0, drawable.getMinimumHeight());
+                    listViews.get(i).setCompoundDrawables(null, drawable, null, null);
+                }
+            }
         }
     }
 
