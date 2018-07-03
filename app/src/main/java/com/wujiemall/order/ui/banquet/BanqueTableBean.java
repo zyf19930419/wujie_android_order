d93c516ac50a6a408acb09c624a2c077fded6c3e
liugang <597749726@qq.com>
2018-07-03 09:37:30 +0800

    2-4-0服务员-宴会台

--- app/src/main/java/com/wujiemall/order/ui/banquet/BanqueTableBean.java
+++ app/src/main/java/com/wujiemall/order/ui/banquet/BanqueTableBean.java
@@ -1,6 +1,7 @@
 package com.wujiemall.order.ui.banquet;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * 创建者：TJDragon(LiuGang)
@@ -11,4 +12,38 @@ import java.io.Serializable;
 public class BanqueTableBean implements Serializable {
     private String data;
     private String name;
+    private String phoneNo;
+    private ArrayList<String> reTableNo;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public ArrayList<String> getReTableNo() {
+        return reTableNo;
+    }
+
+    public void setReTableNo(ArrayList<String> reTableNo) {
+        this.reTableNo = reTableNo;
+    }
 }
--- app/src/main/java/com/wujiemall/order/ui/banquet/BanquetTableActivity.java
+++ app/src/main/java/com/wujiemall/order/ui/banquet/BanquetTableActivity.java
@@ -1,13 +1,20 @@
 package com.wujiemall.order.ui.banquet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.wujiemall.order.R;
 import com.wujiemall.order.base.BaseActivity;
+import com.wujiemall.order.base.BaseRecycleAdapter;
+
+import java.util.ArrayList;
 
 /**
  * 创建者：TJDragon(LiuGang)
@@ -15,8 +22,10 @@ import com.wujiemall.order.base.BaseActivity;
  * 功能描述：
  * 联系方式：常用邮箱或电话
  */
-public class BanquetTableActivity extends BaseActivity implements View.OnClickListener{
+public class BanquetTableActivity extends BaseActivity implements View.OnClickListener, BaseRecycleAdapter.ItemClickListener {
     private RecyclerView banquet_table_Rv;
+    private BanqueTableAdapter banqueTableAdapter;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_banquet_table;
@@ -24,14 +33,29 @@ public class BanquetTableActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     public void initView() {
-        titleSetting("宴会台","添加宴会",this,R.color.title_redF23030);
-        banquet_table_Rv=findViewById(R.id.banquet_table_Rv);
-//        banquet_table_Rv.setAdapter();
+        titleSetting("宴会台", "添加宴会", this, R.color.title_redF23030);
+        banquet_table_Rv = findViewById(R.id.banquet_table_Rv);
+        banqueTableAdapter = new BanqueTableAdapter(BanquetTableActivity.this, BanquetTableActivity.this, null);
+        banquet_table_Rv.setLayoutManager(new LinearLayoutManager(BanquetTableActivity.this, LinearLayoutManager.VERTICAL, false));
+        banquet_table_Rv.setAdapter(banqueTableAdapter);
     }
 
     @Override
     public void initData(Bundle savedInstanceState) {
-
+        ArrayList<BanqueTableBean> banqueTableBeans=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            BanqueTableBean banqueTableBean=new BanqueTableBean();
+            banqueTableBean.setData("2017/04/08  午餐");
+            banqueTableBean.setName("王凯旋");
+            banqueTableBean.setPhoneNo("15081138523");
+            ArrayList<String>  nos=new ArrayList<>();
+            for(int j=0;j<3;j++){
+                nos.add("A-00"+(j+1));
+            }
+            banqueTableBean.setReTableNo(nos);
+            banqueTableBeans.add(banqueTableBean);
+        }
+        banqueTableAdapter.setList(banqueTableBeans);
     }
 
     @Override
@@ -43,26 +67,71 @@ public class BanquetTableActivity extends BaseActivity implements View.OnClickLi
     public void onClick(View v) {
 
     }
-    private class BanqueTableAdapter extends RecyclerView.Adapter{
-        @NonNull
+
+    @Override
+    public void onItemClick(int position, BaseRecycleAdapter adapter) {
+        if (adapter instanceof BanqueTableAdapter) {
+
+        }
+    }
+
+    private class BanqueTableAdapter extends BaseRecycleAdapter<BanqueTableBean> {
+
+        /**
+         * @param context
+         * @param itemClickListener
+         * @param itemLongClickListener
+         */
+        protected BanqueTableAdapter(Context context, ItemClickListener itemClickListener, ItemLongClickListener itemLongClickListener) {
+            super(context, itemClickListener, itemLongClickListener);
+        }
+
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+        public RecyclerView.ViewHolder initHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.item_banquet_table, parent, false);
+            BanqueTableHolder banqueTableHolder = new BanqueTableHolder(view);
+            return banqueTableHolder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            BanqueTableHolder banqueTableHolder = (BanqueTableHolder) holder;
+            BanqueTableBean banqueTableBean = mList.get(position);
+            banqueTableHolder.titleTv.setText(banqueTableBean.getData());
+            banqueTableHolder.name_phoneTv.setText(banqueTableBean.getName() + "\t" + banqueTableBean.getPhoneNo());
 
+            StringBuffer noBuffer = new StringBuffer();
+            int i = 0;
+            for (String no :
+                    banqueTableBean.getReTableNo()) {
+                noBuffer.append(no);
+                if (i != banqueTableBean.getPhoneNo().length() - 1) {
+                    noBuffer.append("、");
+                }
+            }
+            banqueTableHolder.reTableNoTv.setText(Html.fromHtml("<font color=\"#999999\">预定桌位:</font>\t" + "<font color=\"#333333\">" + noBuffer + "</font>"));
+
+            banqueTableHolder.see_detailTv.setTag(position);
+            if (null != itemClickListener) {
+                banqueTableHolder.see_detailTv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        itemClickListener.onItemClick(Integer.parseInt(String.valueOf(v.getTag())), BanqueTableAdapter.this);
+                    }
+                });
+            }
         }
 
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-        private class BanqueTableHolder extends RecyclerView.ViewHolder{
+        private class BanqueTableHolder extends RecyclerView.ViewHolder {
+            private TextView titleTv, name_phoneTv, reTableNoTv, see_detailTv;
 
             public BanqueTableHolder(View itemView) {
                 super(itemView);
+                titleTv = itemView.findViewById(R.id.titleTv);
+                name_phoneTv = itemView.findViewById(R.id.name_phoneTv);
+                reTableNoTv = itemView.findViewById(R.id.reTableNoTv);
+                see_detailTv = itemView.findViewById(R.id.see_detailTv);
+
             }
         }
     }
